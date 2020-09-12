9214f83b325d109f16abbc3e59371b146c5eceb6
Solomon Stevens <haywardhobo@gmail.com>
2020-09-11 18:42:20 -0700

    adding files

---
+++ Assets/__Scripts/FallingBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LearnProgrammingAcademy.AstroAssault
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class FallingBehavior : MonoBehaviour
+    {
+        // == fields ==
+        [SerializeField]
+        private float speed = 2f;
+
+        private Rigidbody2D body;
+
+        // == messages ==
+        private void Start()
+        {
+            body = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            body.velocity = Vector2.down * speed;
+        }
+    }
+}
---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LearnProgrammingAcademy.AstroAssault
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region == constants ==
+        private const string HORIZONTAL = "Horizontal";
+
+        #endregion
+
+        #region == fields ==
+        private Rigidbody2D body;
+
+        #endregion
+        [SerializeField]
+        private float speed = 15f;
+
+        [SerializeField]
+        private float xmin = -7.2f;
+
+        [SerializeField]
+        private float xmax = 7.2f;
+
+        [SerializeField]
+        private float padding = 0.8f;
+
+        #region == messages ==
+        private void Start()
+        {
+            body = GetComponent<Rigidbody2D>();
+        }
+
+        // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
+        private void FixedUpdate()
+        {
+            float horizontalMovement = Input.GetAxis(HORIZONTAL);
+
+            body.velocity = new Vector2(horizontalMovement * speed, 0f);
+
+            // restrict player movement between xmin and xmax
+            float newX = Mathf.Clamp(body.position.x, xmin + padding, xmax - padding);
+            body.position = new Vector2(newX, body.position.y);
+
+        }
+
+        #endregion
+    }
+}
